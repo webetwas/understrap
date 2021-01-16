fbcc3b4afd073f24f940cf01802c49aa03b7c4fc
webetwas <44095814+webetwas@users.noreply.github.com>
2021-01-16 17:44:58 +0200

    add scss original

--- header.php
+++ header.php
@@ -18,11 +18,56 @@ $container = get_theme_mod( 'understrap_container_type' );
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
+
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Dosis:wght@200;300;400;500;600;700;800&family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 	<?php wp_head(); ?>
+
+
+
 </head>
 
 <body <?php body_class(); ?> <?php understrap_body_attributes(); ?>>
 <?php do_action( 'wp_body_open' ); ?>
+
+<div class="container">
+	<div class="row hidden-xs">
+		<div class="col-sm-4 col-sm-4 col-4 text-center img-responsive"><img src="http://cupfocsani.ro/public/assets/img/ue/logo-UE-2018.jpg"></div>
+		<div class="col-sm-4 col-sm-4 col-4 text-center img-responsive"><img src="http://cupfocsani.ro/public/assets/img/ue/logo-GR-2018.jpg"></div>
+		<div class="col-sm-4 col-sm-4 col-4 text-center img-responsive"><img src="http://cupfocsani.ro/public/assets/img/ue/logo-IS-2014-2020.jpg"></div>
+	</div>
+</div>
+
+<div class="padding-tb-5px position-relative" style="background-color:#5091c8;">
+		<div class="container">
+			<div class="row">
+
+				<div class="col-xl-3 col-lg-3 col-3 align align js-equal-8" style="height: 62.4px;">
+					<ul class="list-inline text-center margin-0px text-white pull-left just-a-box ">
+												<li class="list-inline-item unknown-el"><a class="facebook" href="https://www.facebook.com/cupfocsani.ro/" target="_blank"><i class="fa fa-facebook-square fa-3x"></i></a></li>
+																							</ul>
+					<!-- // Social -->
+				</div>
+				<div class="col-xl-6 col-lg-6 d-none d-xl-block js-equal-8" style="height: 62.4px;">
+					<div class="contact-info text-white text-center align">
+						<!-- <span class="margin-right-10px">Tel:  <a href="tel:+40 237 226 400">+40 237 226 400</a></span>
+						<span>Email: <a href="mailto:secretariat@cupfocsani.ro">secretariat@cupfocsani.ro</a></span> -->
+						<h3 style="letter-spacing: 3px;">COMPANIA DE UTILITĂȚI PUBLICE FOCȘANI</h3><h5>operator regional al serviciilor de apă și canalizare</h5>
+					</div>
+				</div>
+				<div class="col-xl-3 col-9 js-equal-8 no-pad-col" style="height: 62.4px;">
+					<div class="container just-a-box">
+						<div class="unknown-el">
+							<?php echo get_search_form();?>
+						</div>
+
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
 <div class="site" id="page">
 
 	<!-- ******************* The Navbar Area ******************* -->

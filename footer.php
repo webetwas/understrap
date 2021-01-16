0230f16391953462070fdcef62921ec3c7ee7f78
webetwas <44095814+webetwas@users.noreply.github.com>
2021-01-17 00:13:22 +0200

    add style footer

--- footer.php
+++ footer.php
@@ -41,7 +41,38 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 </div><!-- wrapper end -->
 
+<section class="background-light-grey">
+	<div class="container">
+		<div class="row padding-tb-25px line-ue">
+			<div class="col-lg-6">
+				<span style="text-align: justify;">
+						Proiect cofinanțat din Fondul de Coeziune prin Programul Operațional Infrastructura Mare 2014-2020.
+						Conținutul acestui material nu reprezintă în mod obligatoriu poziția oficiala a Uniunii Europene  sau a Guvernului României.
+				</span>
+			</div>
+			<div class="col-lg-6">
+				<span style="text-align: justify;">Pentru informații detaliate despre celelalte programe cofinanțate de Uniunea Europeană, vă invităm să vizitați
+				<a href="www.fonduri-ue.ro" target="_blank" class="link-ue">www.fonduri-ue.ro</a></span>
+			</div>
+		</div>
+	</div>
+
+</section>
+
 </div><!-- #page we need this extra closing tag here -->
+
+<div class="padding-tb-5px position-relative" style="background-color:#5091c8; ">
+	<div class="container">
+		<div class="row wow fadeInUp">
+				<div class="col-lg-12">
+					<span class="text-white d-block padding-top-5px text-center">
+                        Compania De Utilitati Publice Focsani &copy <?php echo date(Y);?> | Toate drepturile rezervate
+                    </span>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 
 <?php wp_footer(); ?>
 
